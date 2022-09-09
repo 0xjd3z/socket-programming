c4ae6e2fb0afcedfd602d8fc8770acc7140ef9e5
0xjdez <jimroxodezi@gmail.com>
2022-09-09 17:28:26 +0100

    set socket options

--- tcp/server.py
+++ tcp/server.py
@@ -3,19 +3,20 @@ import socket
 SERVER_NAME = "localhost"
 SERVER_PORT = 1234
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((SERVER_NAME, SERVER_PORT))
-server.listen(1)
-print("The server is ready to recieve.")
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+    server.bind((SERVER_NAME, SERVER_PORT))
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.listen(1)
+    print("The server is ready to recieve.")
 
-while True:
-    #accept() creates a new socket for communicating with client
-    conn_socket, addr = server.accept()
-    print("accepted connection from", addr)
-    message = conn_socket.recv(1024).decode()
+    while True:
+        #accept() creates a new socket for communicating with client
+        conn_socket, addr = server.accept()
+        print("accepted connection from", addr)
+        message = conn_socket.recv(1024).decode()
 
-    modified_message = message.upper()
-    conn_socket.send(modified_message.encode())
-    conn_socket.close()
+        modified_message = message.upper()
+        conn_socket.send(modified_message.encode())
+        conn_socket.close()
 
 
